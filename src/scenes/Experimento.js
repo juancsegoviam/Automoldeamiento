72a4fcaa324e0cf0ee771e91f50976c0f6715c11
juancsegovia <46732662+juancsegovia@users.noreply.github.com>
2021-10-29 00:43:25 -0700

    vamos a ver

--- src/scenes/Experimento.js
+++ src/scenes/Experimento.js
@@ -54,10 +54,10 @@ export default class Experimento extends Phaser.Scene
 {
     preload()
     {
-        this.load.image('bullet', 'C:/Users/Juan Carlos/Desktop/Automoldeamiento/assets/bullet.png');
-        this.load.image('player', 'C:/Users/Juan Carlos/Desktop/Automoldeamiento/assets/player.png');
-        this.load.spritesheet('cent', 'C:/Users/Juan Carlos/Desktop/Automoldeamiento/assets/cent.png', { frameWidth: 50 , frameHeight: 50 });
-        this.load.spritesheet('stimulus', 'C:/Users/Juan Carlos/Desktop/Automoldeamiento/assets/sti.png', { frameWidth: 50 , frameHeight: 50 });
+        //this.load.image('bullet', 'C:/Users/Juan Carlos/Desktop/Automoldeamiento/assets/bullet.png');
+        this.load.image('player', 'assets/player.png');
+        //this.load.spritesheet('cent', 'C:/Users/Juan Carlos/Desktop/Automoldeamiento/assets/cent.png', { frameWidth: 50 , frameHeight: 50 });
+        //this.load.spritesheet('stimulus', 'C:/Users/Juan Carlos/Desktop/Automoldeamiento/assets/sti.png', { frameWidth: 50 , frameHeight: 50 });
     }
 
     create()

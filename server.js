2ef7be8717ed0383b581387b69a9538cbaf46dd5
juancsegovia <46732662+juancsegovia@users.noreply.github.com>
2021-10-29 01:54:39 -0700

    meta intetno

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const app = express();
 const DIST_DIR = path.join(__dirname, '/dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
+
+app.use(express.static('public'))
 app.use(express.static(DIST_DIR));
 app.get('*', (req, res) => {
   res.sendFile(HTML_FILE);

1c6a8906c1c620a03d1b920cbd6e1ad5c8e9db39
juancsegovia <46732662+juancsegovia@users.noreply.github.com>
2021-10-28 23:10:45 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: ['./src/index.js','./src/consts/SceneKeys.js','/src/scenes/Tutorial.js'],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),

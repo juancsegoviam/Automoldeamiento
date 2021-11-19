53b468cc0b273d97860340b95d56dff887a2a2a3
juancsegovia <46732662+juancsegovia@users.noreply.github.com>
2021-11-19 01:30:35 -0600

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const DIST_DIR = path.join(__dirname, '/dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
 
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "public")))
 app.use(express.static(DIST_DIR));
 app.get('*', (req, res) => {
   res.sendFile(HTML_FILE);

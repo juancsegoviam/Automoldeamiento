ea397fe050c70be421993c67c0205297c3938c6d
juancsegovia <46732662+juancsegovia@users.noreply.github.com>
2022-01-12 15:49:01 -0600

    Agregar los env para que jale

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const path = require('path');
 const express = require('express');
+const mongoose = require('mongoose');
+const password = require('./password.js');
+require('dotenv').config();
 
 const app = express();
 const DIST_DIR = path.join(__dirname, '/dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html');
+
+const connectionString = `mongodb+srv://CEICUDG:5HxKswzBdzBq%40EY@autoshaping0.6vxnd.mongodb.net/datos?retryWrites=true&w=majority`;
+
+mongoose.connect(process.env.MONGO_URL, {
+ 
+})
+.then(() => {
+    console.log('Database connected')
+}).catch(err => {
+    console.error('Unsuccesfull connect',err)
+})
 
 
 app.use(express.static(path.join(__dirname, "public")))
